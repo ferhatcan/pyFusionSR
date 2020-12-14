580f556d88a5378edb307673942b057a7db3f3a1
ferhatcan <ataman.ferhatcan@gmail.com>
2020-12-14 11:42:06 +0300

    config file given in these part (previously hard coded in assembler) beacuse there are different config files that uses same assembler for testing

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 from assemblers.assemblerGetter import getExperimentWithDesiredAssembler
 
 DESIRED_ASSEMBLER = "fusionv2ADAS"
+CONFIG_FILE_NAME = "./configs/encoderDecoderFusionv2ADAS_YsingleChannels.ini"
 
 def main():
-    experiment = getExperimentWithDesiredAssembler(DESIRED_ASSEMBLER)
-    # experiment.load('model_last')
+    experiment = getExperimentWithDesiredAssembler(DESIRED_ASSEMBLER, CONFIG_FILE_NAME)
+    experiment.load('model_last')
     experiment.train()
 
 if __name__ == '__main__':
