a3e7ea3d6a2d6fb73b3ae298a027ecebb29ca884
ferhatcan <ataman.ferhatcan@gmail.com>
2020-12-20 15:25:32 +0300

    relative paths to work on each device without any problem

--- tests/compareMethods.m
+++ tests/compareMethods.m
@@ -9,14 +9,14 @@ addpath('./imageFusionMetrics-master/')
 
 %% Load input images
 
-INPUT_IMAGE_PATH = '/media/ferhatcan/common/Image_Datasets/VIFB-master/input/';
+INPUT_IMAGE_PATH = '../../../Image_Datasets/VIFB-master/input/';
 
 vis_images = dir(fullfile(INPUT_IMAGE_PATH, 'VI/*.jpg'));
 ir_images = dir(fullfile(INPUT_IMAGE_PATH, 'IR/*.jpg'));
 
 %% Find method names
 
-FUSE_IMAGE_PATH = '/media/ferhatcan/common/Image_Datasets/VIFB-master/fused_images/';
+FUSE_IMAGE_PATH = '../../../Image_Datasets/VIFB-master/fused_images/';
 
 fuse_images = dir(fullfile(FUSE_IMAGE_PATH, '*.jpg'));
 image_name = ir_images(1).name(1:end-4);
@@ -32,9 +32,13 @@ end
 total_result = zeros(size(method_names, 2), 12);
 
 for i = 1:size(method_names, 2)
+   disp('-------------------');
+   fprintf(method_names{i});
    relevant_fuse_images = fuse_images(contains({fuse_images.name}, strcat(method_names(i), '.jpg')));  
    [curr_result, names] = evaluateAllPairs(relevant_fuse_images, ir_images, vis_images, method_names(i));
-   total_result(i, :) = sum(curr_result) / size(names, 2);
+   total_result(i, :) = sum(curr_result) / size(total_results, 1);
+   disp('FINISHED....')
+   disp('');
 end
 
 %% Save Results (run time is too much)
