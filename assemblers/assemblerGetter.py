70d94c68607ea443a367ba78ff80d4ce718c7a41
ferhatcan <ataman.ferhatcan@gmail.com>
2020-12-20 15:07:01 +0300

    new flirADAS + KAIST combined type assembler added.

--- assemblers/assemblerGetter.py
+++ assemblers/assemblerGetter.py
@@ -1,10 +1,13 @@
 from assemblers.encoderDecoderFusionv2Assembler import getExperiment as fusionv2
 from assemblers.encoderDecoderFusionv2ADASAssembler import getExperiment as fusionv2adas
+from assemblers.encoderDecoderFusionv2ADASKAISTAssembler import getExperiment as fusionadaskaist
 
 def getExperimentWithDesiredAssembler(name, config_file):
     if name == "fusionv2":
         return fusionv2(config_file)
     elif name == "fusionv2ADAS":
         return fusionv2adas(config_file)
+    elif name == "fusionv2ADASKAIST":
+        return fusionadaskaist(config_file)
     else:
         assert 1 == 0, "there should be invalid assembler name"
---
+++ assemblers/encoderDecoderFusionv2ADASKAISTAssembler.py
@@ -0,0 +1,50 @@
+from options import options
+from utils.benchmark import BenchmarkMetrics
+
+from dataloaders.dataloaderGetters import *
+import models.encoderdecoder as model
+from utils.logger import LoggerTensorBoard
+from loss.lossGetters import *
+from methods.methodGetters import *
+from experiments.experimentGetters import *
+from optimizers.optimizerSchedulerGetters import *
+
+def getExperiment(config_file):
+    CONFIG_FILE_NAME = config_file
+
+    args = options(CONFIG_FILE_NAME)
+    print("The system will use following resource: {:}".format(args.argsCommon.device))
+    print("Experiment Name: " + args.argsCommon.experiment_name)
+    print("Experiment will be saved to " + args.argsCommon.experiment_save_path)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() and args.argsCommon.device == "gpu" else "cpu")
+
+    dataloaders = getFlirAdasKaistCombinedDataLoaders(args.argsDataset)
+
+    currentModel = model.make_model(args.argsModel)
+    currentModel.to(device)
+
+    possibles = globals().copy()
+    loss_dict = dict()
+    loss_dict["types"] = []
+    loss_dict["functions"] = []
+    loss_dict["weights"] = []
+    for loss in args.argsLoss.loss.split('+'):
+        weight, loss_type = loss.split('*')
+        loss_dict["functions"].append(possibles.get('get'+loss_type+'Loss')(args.argsLoss))
+        loss_dict["weights"].append(float(weight))
+        loss_dict["types"].append(loss_type)
+    loss_dict["types"].append('total')
+
+    lr_scheduler, optimizer = possibles.get('get'+args.argsMethod.optimizer+'Optimizer')(currentModel.parameters(), args.argsMethod)
+
+    method = getBaseMethod(currentModel, loss_dict, optimizer, args.argsMethod)
+
+    benchmark = BenchmarkMetrics(args.argsBenchmark)
+
+    logger = LoggerTensorBoard(args.argsCommon.experiment_save_path,
+                               args.argsCommon.experiment_save_path + '/tensorboard')
+
+    experiment = getBaseExperiment(currentModel, dataloaders, loss_dict, method, optimizer, lr_scheduler, benchmark, logger, args.argsExperiment)
+
+    return experiment
