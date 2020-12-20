3afa2f38548e933681c8fe00ec6b8e090eec502d
ferhatcan <ataman.ferhatcan@gmail.com>
2020-12-20 15:20:00 +0300

    save result uses new parameters added to config

--- saveResult.py
+++ saveResult.py
@@ -19,9 +19,12 @@ import os
 from assemblers.assemblerGetter import getExperimentWithDesiredAssembler
 
 DESIRED_ASSEMBLER = "fusionv2ADAS"
-CONFIG_FILE_NAME = "./configs/encoderDecoderFusionv2ADAS_YsingleChannels.ini"
+CONFIG_FILE_NAME = "./configs/encoderDecoderFusionv2ADAS_HSVsingleChannel.ini"
 ROOT_PATH = "/media/ferhatcan/common/Image_Datasets/VIFB-master/input/"
-OUTPUT_PATH = "./Outputs/IV_images/"
+OUTPUT_PATH = "./Outputs/HSV+IR_2/"
+
+which_channel = 2
+channel_type = 'HSV'
 
 def extract_image_files():
     extensions = ["jpg", "jpeg", "png"]
@@ -46,17 +49,23 @@ def main():
     experiment.load('model_last')
 
     imageFiles = extract_image_files()
+    os.makedirs(OUTPUT_PATH, exist_ok=True)
+
+
     for visible_filename, ir_filename in zip(imageFiles["visible"], imageFiles["thermal"]):
         visible = Image.open(visible_filename)
         ir = Image.open(ir_filename)
 
         ir = ir.convert('L')
         # visible = visible.convert('RGB')
-        ycbcr = visible.convert('YCbCr')
+        ycbcr = visible.convert(channel_type)
         B = np.ndarray((visible.size[1], visible.size[0], 3), 'u1', ycbcr.tobytes())
-        visible = Image.fromarray(B[:, :, 0], 'L')
+        visible = Image.fromarray(B[:, :, which_channel], 'L')
 
-        resize = transforms.Resize(size=[480, 640], interpolation=Image.BICUBIC)
+        original_size = [visible.size[1], visible.size[0]]
+        desiredSize = [value - (value % 32) for value in original_size]
+
+        resize = transforms.Resize(size=desiredSize, interpolation=Image.BICUBIC)
         ir = resize(ir)
         visible = resize(visible)
         ycbcr = resize(ycbcr)
@@ -70,20 +79,22 @@ def main():
         data["inputs"] = [lr_image.unsqueeze(dim=0), hr_image.unsqueeze(dim=0)]
 
         output = experiment.inference(data)
-        data["result"] = [kn.normalize_min_max(output["results"][0])]
-
+        # data["result"] = [kn.normalize_min_max(output["results"][0])]
+        data["result"] = [output["results"][0]]
+        # print(data["result"][0].cpu().detach().numpy().max(), data["result"][0].cpu().detach().numpy().min())
         out = np.copy(B)
-        out[:, :, 0] = data["result"][0].cpu().detach().numpy() * (235 - 16) + 16
-        outPil = Image.fromarray(out, 'YCbCr')
+        out[:, :, which_channel] = data["result"][0].cpu().detach().numpy() * 255
+        outPil = Image.fromarray(out, channel_type)
         outPil = outPil.convert('RGB')
+
+        resize_original = transforms.Resize(size=original_size, interpolation=Image.BICUBIC)
+        out_final = resize_original(outPil)
 
         saveName = os.path.split(ir_filename)[-1]
         saveName, ext = saveName.split('.')
-        saveName = saveName + '_fused.' + ext
+        saveName = saveName + '_proposed.' + ext
 
-        os.makedirs(OUTPUT_PATH, exist_ok=True)
-
-        outPil.save(os.path.join(OUTPUT_PATH, saveName))
+        out_final.save(os.path.join(OUTPUT_PATH, saveName))
 
 if __name__ == '__main__':
     main()
