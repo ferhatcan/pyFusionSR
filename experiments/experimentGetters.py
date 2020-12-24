620f69f9109a6bcddc4a3c3bd3d84faaa89b2d72
ferhatcan <ataman.ferhatcan@gmail.com>
2020-12-24 08:36:39 +0300

    multiple dataloader training and testing experiment is added

--- experiments/experimentGetters.py
+++ experiments/experimentGetters.py
@@ -1,4 +1,8 @@
 from experiments.baseExperiment import BaseExperiment
+from experiments.multipleDataloaderExperiment import MultipleDataloaderExperiment
 
 def getBaseExperiment(model, dataLoaders, loss, method, optimizer, lr_scheduler, benchmark, logger, args):
     return BaseExperiment(model, dataLoaders, loss, method, optimizer, lr_scheduler, benchmark, logger, args)
+
+def getMultipleDataloaderExperiment(model, dataLoaders, loss, method, optimizer, lr_scheduler, benchmark, logger, args):
+    return MultipleDataloaderExperiment(model, dataLoaders, loss, method, optimizer, lr_scheduler, benchmark, logger, args)
