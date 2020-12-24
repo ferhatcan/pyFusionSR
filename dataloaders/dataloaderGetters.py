4277d46e59e8135f92e7cd3b276ad8a94ab6ff5d
ferhatcan <ataman.ferhatcan@gmail.com>
2020-12-24 08:25:00 +0300

    random sampler dataset is removed. (subset method is used instead) num_worker > 0 to load images fast. KAIST and ADAS dataloader seperate function is added.

--- dataloaders/dataloaderGetters.py
+++ dataloaders/dataloaderGetters.py
@@ -14,12 +14,19 @@ def _generateDataLoaders(ds_train, ds_test, batch_size, validation_percent=0.1, 
         np.random.shuffle(indices)
     train_indices, val_indices = indices[split:], indices[:split]
 
-    train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_indices)
-    validation_sampler = torch.utils.data.sampler.SubsetRandomSampler(val_indices)
+    valid_ds = torch.utils.data.Subset(ds_train, val_indices)
+    train_ds = torch.utils.data.Subset(ds_train, train_indices)
 
-    loader_train = torch.utils.data.DataLoader(ds_train, batch_size=batch_size, sampler=train_sampler)
-    loader_val = torch.utils.data.DataLoader(ds_train, batch_size=batch_size, sampler=validation_sampler)
-    loader_test = torch.utils.data.DataLoader(ds_test, batch_size=batch_size, shuffle=False)
+    # train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_indices)
+    # validation_sampler = torch.utils.data.sampler.SubsetRandomSampler(val_indices)
+    #
+    # loader_train = torch.utils.data.DataLoader(ds_train, batch_size=batch_size, sampler=train_sampler, num_workers=0)
+    # loader_val = torch.utils.data.DataLoader(ds_train, batch_size=batch_size, sampler=validation_sampler, num_workers=0)
+    # loader_test = torch.utils.data.DataLoader(ds_test, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    loader_train = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, num_workers=16)
+    loader_val = torch.utils.data.DataLoader(valid_ds, batch_size=batch_size, num_workers=16)
+    loader_test = torch.utils.data.DataLoader(ds_test, batch_size=batch_size, num_workers=8)
 
     loaders = dict()
     loaders["train"] = loader_train
@@ -61,5 +68,22 @@ def getFlirAdasKaistCombinedDataLoaders(args):
     ds_test = torch.utils.data.ConcatDataset([ds_test_KAIST, ds_test_Flir])
 
     return _generateDataLoaders(ds_train, ds_test, args.batch_size, args.validation_size, args.shuffle_dataset)
+
+def getFlirAdasKaistMultipleDataLoaders(args):
+    train_set_paths = args.train_set_paths
+    test_set_paths = args.test_set_paths
+    args.train_set_paths = [train_set_paths[0]]
+    args.test_set_paths = [test_set_paths[0]]
+    ds_train_KAIST = KaistDataset(args=args, train=True)
+    ds_test_KAIST = KaistDataset(args=args, train=False)
+    args.train_set_paths = [train_set_paths[1]]
+    args.test_set_paths = [test_set_paths[1]]
+    ds_train_Flir = FlirAdasDataset(args=args, train=True)
+    ds_test_Flir = FlirAdasDataset(args=args, train=False)
+
+    set1_dl = _generateDataLoaders(ds_train_KAIST, ds_test_KAIST, args.batch_size, args.validation_size, args.shuffle_dataset)
+    set2_dl = _generateDataLoaders(ds_train_Flir, ds_test_Flir, args.batch_size, args.validation_size, args.shuffle_dataset)
+
+    return [set1_dl, set2_dl]
 
 
