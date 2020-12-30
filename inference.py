d0bf4afe472ec7d154017a57d4542affde3e1a0d
ferhatcan <ataman.ferhatcan@gmail.com>
2020-12-30 07:57:49 +0300

    it works in between -1,1 range images

--- inference.py
+++ inference.py
@@ -1,4 +1,5 @@
 import PIL.Image as Image
+from skimage import color
 from torchvision.transforms import functional as tvF
 import torchvision.transforms as transforms
 import torch
@@ -9,11 +10,11 @@ from utils.benchmark import BenchmarkMetrics
 import os
 from assemblers.assemblerGetter import getExperimentWithDesiredAssembler
 
-DESIRED_ASSEMBLER = "fusionv2ADAS"
-CONFIG_FILE_NAME = "./configs/encoderDecoderFusionv2ADAS_HSVsingleChannel.ini"
+DESIRED_ASSEMBLER = "fusionv2ADASKAIST"
+CONFIG_FILE_NAME = "./configs/encoderDecoderFusionv2ADAS+KAIST_YsingleChannels.ini"
 
-which_channel = 2
-channel_type = 'HSV'
+which_channel = 0
+channel_type = 'YCbCr'
 
 def imshow(imageList):
     for i in range(len(imageList)):
@@ -24,13 +25,15 @@ def main():
     experiment = getExperimentWithDesiredAssembler(DESIRED_ASSEMBLER, CONFIG_FILE_NAME)
     experiment.load('model_last')
 
+    # validation_loss, validation_benchmark, _ = experiment.test_dataloader(experiment.dataloaders["validation"])
+
     # rootPath = '/media/ferhatcan/common/Image_Datasets/rgbt-ped-detection/data/kaist-rgbt/images/set01/V000/'
     # rootPath = '/media/ferhatcan/common/Image_Datasets/TNO_Image_Fusion_Dataset/Triclobs_images/Bosnia/'
     rootPath = '/media/ferhatcan/common/Image_Datasets/VIFB-master/input/'
     # rootPath = '/media/ferhatcan/common/Image_Datasets/Flir/UPDATE 8-19-19_ SB Free Dataset-selected/FLIR_ADAS_1_3/val/'
 
-    visible = Image.open(os.path.join(rootPath, 'VI/snow.jpg'))
-    ir = Image.open(os.path.join(rootPath, 'IR/snow.jpg'))
+    visible = Image.open(os.path.join(rootPath, 'VI/carWhite.jpg'))
+    ir = Image.open(os.path.join(rootPath, 'IR/carWhite.jpg'))
     ir = ir.convert('L')
     # visible = visible.convert('RGB')
     color_im = visible.convert(channel_type)
@@ -52,13 +55,18 @@ def main():
     hr_image = tvF.to_tensor(visible)
     lr_image = tvF.to_tensor(ir)
 
-    # imshow([lr_image.unsqueeze(dim=0), lr_image.unsqueeze(dim=0)])
+    hr_image = hr_image * 2 - 1
+    lr_image = lr_image * 2 - 1
+
+    imshow([(lr_image.unsqueeze(dim=0) + 1) * 0.5, (hr_image.unsqueeze(dim=0) + 1) * 0.5])
 
     data = dict()
     data["inputs"] = [lr_image.unsqueeze(dim=0), hr_image.unsqueeze(dim=0)]
 
 
     output = experiment.inference(data)
+    # data["result"] = [torch.clamp(output["results"][0], 0, 1)]
+    data["result"] = [(output["results"][0] + 1) * 0.5]
     # data["result"] = [kn.normalize_min_max(output["results"][0])]
     data["gts"] = data["inputs"]
 
@@ -72,16 +80,32 @@ def main():
     vis_inp = tvF.to_tensor(vis_inp).unsqueeze(dim=0)
     # imshow([lr_image.unsqueeze(dim=0), hr_image.unsqueeze(dim=0), data["result"][0].cpu().detach()])
 
-    data["result"] = [output["results"][0]]
 
-    print(B[:, :, 2].max(), B[:, :, 2].min())
+    # data["result"][0] = data["result"][0] * 255
+
+    print(B[:, :, which_channel].max(), B[:, :, which_channel].min())
     print(data["result"][0].cpu().detach().numpy().max(), data["result"][0].cpu().detach().numpy().min())
     out = np.copy(B)
-    out[:, :, which_channel] = data["result"][0].cpu().detach().numpy() * 255
+    out[:, :, which_channel] = data["result"][0].cpu().squeeze().detach().numpy() * 255
+    # print(out[:, :, 2].max(), out[:, :, 2].min())
+    # out_np = np.array(out, dtype=np.uint8)
     outPil = Image.fromarray(out, channel_type)
     outPil = outPil.convert('RGB')
 
+    # inpImage = Image.open(os.path.join(rootPath, 'VI/labMan.jpg')).convert('RGB')
+    # np_inpImage = np.array(inpImage, dtype=np.float32) / 255
+    # B_lab = color.rgb2lab(np_inpImage)
+    #
+    # B_lab[:, :, 0] = 100
+
+    # print(B_lab[:, :, 0].max(), B_lab[:, :, 0].min())
+    # B_rgb = color.lab2rgb(B_lab)
+    # print(B_rgb[:, :, 0].max(), B_rgb[:, :, 0].min())
+    # outPil = Image.fromarray((B_rgb[:, :, 0] * 255).astype(np.uint8), 'L').convert('RGB')
+
     out_final = resize_original(outPil)
+
+    # imshow([tvF.to_tensor(out_final.convert('L')).unsqueeze(dim=0), (lr_image.unsqueeze(dim=0) + 1) * 0.5, (hr_image.unsqueeze(dim=0) + 1) * 0.5, data["result"][0].cpu().detach()])
 
     out_final = tvF.to_tensor(out_final).unsqueeze(dim=0)
 
